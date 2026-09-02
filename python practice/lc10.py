2731cc57cd33f3700f0055addb5ca310c9967448
codefluxorigin <amit126223@gmail.com>
2026-09-02 23:02:24 +0530

    Add function to determine if a number is even or odd and handle user input

---
+++ python practice/lc10.py
@@ -0,0 +1,10 @@
+def num(a):
+    if a%2==0:
+        print("the number is even")
+    else:
+        print("the number is odd")
+
+    return(a) 
+a=int(input("enater a number"))
+num_1=num(a)
+print(num_1)
