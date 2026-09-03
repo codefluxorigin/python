4e025315d3eba27dfd551ba6269a41dcf3edced4
codefluxorigin <amit126223@gmail.com>
2026-09-03 20:51:04 +0530

    Add new script to define a list of girls and print their elements; create a temporary runner file

---
+++ python practice/lc12.py
@@ -0,0 +1,7 @@
+num=[565,96656,465,546,546,6,6,6,6,6,6,6,6,6,6,526,96,5962,65,6,6,2626]
+girls=["djhfgyjhsdg",'d',"hgfjdsf","dfuyjg",'dfgdf','fgdhfb','dfgsdhfv',"sdfghjkbsd","fhdfgdm","mdjkfvd","jbkkjdgf"]
+def list1(list):
+    for item in list :
+        print(item,end=" ")
+list1(girls)
+print()
