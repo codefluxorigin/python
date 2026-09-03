c6335695d1d465277be2445297cd4715a5251cbc
codefluxorigin <amit126223@gmail.com>
2026-09-03 19:58:42 +0530

    Add initial script to define lists and print their lengths

---
+++ python practice/lc11.py
@@ -0,0 +1,7 @@
+num=[534,56456,65464,54,5645656456,65,4,6,686,6,6,56,66,56,565,56,465,66,6,]
+flowers=["amit" ,"dfghsd","djhfgf","kdjghil","juigh","krhflkrfgflhfr","gfuisrgleuohf","srfg"]
+def  list1(list):
+    print(len(list))
+    return(list)
+list1(num)
+list1(flowers)
