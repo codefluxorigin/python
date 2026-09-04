3470610b159d7382e9d355af5156d8c457b1047b
codefluxorigin <amit126223@gmail.com>
2026-09-04 20:58:33 +0530

    Refactor currency conversion function and remove unused list elements

--- python practice/lc15.py
+++ python practice/lc15.py
@@ -1,4 +1,6 @@
-list1=["amit","sumit","aditya","abhishek"]
-def elemnets(list):
-    for elment in list:
-        print(elmets=)
+def usd(a):
+    inr=a*89
+    return (inr)
+a=float(input("enter amount="))
+inr=usd(a)
+print("amount in rs=",inr)
