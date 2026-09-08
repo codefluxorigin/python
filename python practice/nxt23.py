a92f8ad6d3325d8d85bd325874133916ae714aed
codefluxorigin <amit126223@gmail.com>
2026-09-08 09:37:04 +0530

    Fix grade boundaries to include equal percentages for A+, A, B+, B, and C

--- python practice/nxt23.py
+++ python practice/nxt23.py
@@ -5,15 +5,15 @@ p=float(input("enetr the marks of physic="))
 c=float(input("enetr the marks of chemistry="))
 b=float(input("enetr the marks of bio="))
 percentage=((((m+e+p+c+b))/500)*100)
-if percentage>90:
+if percentage>=90:
     print("grade A+")
-elif percentage>80 and percentage<90:
+elif percentage>=80 and percentage<90:
         print("the grade is A")
-elif percentage>70 and percentage<80:
+elif percentage>=70 and percentage<80:
             print("the grade is B+")
-elif percentage>60 and percentage<70:
+elif percentage>=60 and percentage<70:
                 print("The grade is B")
-elif percentage>50 and percentage<60:
+elif percentage>=50 and percentage<60:
                     print("thr grade is C")
 else:
     print("you are fail")
